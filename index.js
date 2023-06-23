91d9cf84b9f00be0671aecf744e50002c18d7060
chuntikab <chuntika.bum@gmail.com>
2023-06-23 09:36:45 +0700

    autorun

--- index.js
+++ index.js
@@ -103,6 +103,62 @@ app.get('/autorun', (req, res) => {
     });
 });
 
+app.post('/autorunupdate', (req, res) => {
+    const str="Query More - ";
+    // res.send(str);
+
+    var conn = new jsforce.Connection({
+        // you can change loginUrl to connect to sandbox or prerelease env.
+        loginUrl: 'https://test.salesforce.com'
+    });
+    var username = 'sfdc-thaioil.r2@roundtwosolutions.com.devapex';
+    var password = 'sfdc@r22020';
+
+    conn.login(username, password, function (err, userInfo) {
+
+        if (err) {return console.error(err);}
+        console.log("accessToken: "+ conn.accessToken);
+        console.log("instanceUrl: "+ conn.instanceUrl);
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+
+        /*var records = [];
+        var query = conn.query("SELECT Id, Name__c,CreatedBy.Name FROM PTW_Inspection_Report__c WHERE Id=\'a22N0000001s2iCIAQ\'")
+        .on("record", function(record) {
+            records.push(record);
+        })
+        .on("end", function() {
+            console.log("total in database : " + query.totalSize);
+            console.log("total fetched : " + query.totalFetched);
+            // console.log("total records : " + JSON.stringify(records));
+
+            for(var i = 0 ; i < records.length ; i++ )
+            {
+                // console.log(records[0].CreatedBy.Name);
+                records[i].Name__c = 'heroku'+records[i].Id;
+            }
+            res.send(str+records[0].Id);*/
+            conn.sobject("PTW_Inspection_Report__c").update(req.body,
+
+                function(err, rets) {
+                  if (err) { return console.error(err); }
+                  for (var i=0; i < rets.length; i++) {
+                    if (rets[i].success) {
+                      console.log("Updated Successfully : " + rets[i].id);
+                      res.send("Updated Successfully");
+                    }
+                  }
+            });
+        /*})
+        .on("error", function(err) {
+            console.error(err);
+        })
+        .run({ autoFetch : true, maxFetch : 1000000 });*/
+
+        // res.send(str+" done");
+    });
+});
+
 //Sol2
 /*app.get('/', (req, res) => {
     const str="Query More";
