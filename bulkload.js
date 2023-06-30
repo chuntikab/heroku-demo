0c8a921309ae6a4d2685f0c580e2f49c9c638c0c
chuntikab <chuntika.bum@gmail.com>
2023-06-30 14:36:16 +0700

    'add_another_interface'

--- bulkload.js
+++ bulkload.js
@@ -11,7 +11,104 @@ var http = require('http'); //Adding http
 var jsforce = require('jsforce'); //Adding JsForce
 let Queue = require('bull');
  
-//Solution 2
+
+//Solution 2 interface ver.2 - use conn.sobject(YourObject).bulkload("insert").execute()
+app.get('/bulkload_v2-2', (req, res) => {
+    const str="Query More - ";
+
+    var conn = new jsforce.Connection({
+        // you can change loginUrl to connect to sandbox or prerelease env.
+        loginUrl: 'https://login.salesforce.com'
+    });
+    var username = 'chuntika.bum@resilient-goat-reze1m.com';
+    var password = 'TrailHead1007ScMCLFfUPVaYKOTrXoih755C2';
+
+    conn.login(username, password, function (err, userInfo) {
+
+        if (err) {return console.error(err);}
+        console.log("accessToken: "+ conn.accessToken);
+        console.log("instanceUrl: "+ conn.instanceUrl);
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+
+            // Provide records
+            var ptws = [];
+            for (var i = 0; i < 10000 ; i++) {
+                var data = {
+                    Name__c: i+'-r2'
+                };
+                //console.log(records[i].Name);
+                ptws.push(data);
+                //console.log(olist);
+            }
+            conn.bulk.pollTimeout = 25000; // Bulk timeout can be specified globally on the connection object
+
+            conn.sobject("PTW_Inspection_Report__c").bulkload("insert").execute(ptws, function(err, rets) {
+                if (err) { return console.error(err); }
+                for (var i=0; i < rets.length; i++) {
+                  if (rets[i].success) {
+                    console.log("#" + (i+1) + " loaded successfully, id = " + rets[i].id);
+                  } else {
+                    console.log("#" + (i+1) + " error occurred, message = " + rets[i].errors.join(', '));
+                  }
+                }
+                // ...
+            });
+
+            res.send("bulkload_v2-2");
+
+    });
+});
+
+//Solution 2 interface ver.1 - use conn.sobject(YourObject).insertBulk()
+app.get('/bulkload_v2-1', (req, res) => {
+    const str="Query More - ";
+
+    var conn = new jsforce.Connection({
+        // you can change loginUrl to connect to sandbox or prerelease env.
+        loginUrl: 'https://login.salesforce.com'
+    });
+    var username = 'chuntika.bum@resilient-goat-reze1m.com';
+    var password = 'TrailHead1007ScMCLFfUPVaYKOTrXoih755C2';
+
+    conn.login(username, password, function (err, userInfo) {
+
+        if (err) {return console.error(err);}
+        console.log("accessToken: "+ conn.accessToken);
+        console.log("instanceUrl: "+ conn.instanceUrl);
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+
+            // Provide records
+            var ptws = [];
+            for (var i = 0; i < 10000 ; i++) {
+                var data = {
+                    Name__c: i+'-r2'
+                };
+                //console.log(records[i].Name);
+                ptws.push(data);
+                //console.log(olist);
+            }
+            conn.bulk.pollTimeout = 25000; // Bulk timeout can be specified globally on the connection object
+
+            conn.sobject("PTW_Inspection_Report__c").insertBulk(ptws, function(err, rets) {
+                if (err) { return console.error(err); }
+                for (var i=0; i < rets.length; i++) {
+                  if (rets[i].success) {
+                    console.log("#" + (i+1) + " loaded successfully, id = " + rets[i].id);
+                  } else {
+                    console.log("#" + (i+1) + " error occurred, message = " + rets[i].errors.join(', '));
+                  }
+                }
+                // ...
+            });
+
+            res.send("bulkload_v2-1");
+
+    });
+});
+
+//Solution 2 - use conn.bulk.load
 app.get('/bulkload_v2', (req, res) => {
     const str="Query More - ";
 
@@ -41,7 +138,7 @@ app.get('/bulkload_v2', (req, res) => {
                 //console.log(olist);
             }
             conn.bulk.pollTimeout = 25000; // Bulk timeout can be specified globally on the connection object
-            conn.bulk.load("Account", "insert", accounts, function(err, rets) {
+            conn.bulk.load("PTW_Inspection_Report__c", "insert", ptws, function(err, rets) {
               if (err) { return console.error(err); }
               for (var i=0; i < rets.length; i++) {
                 if (rets[i].success) {
@@ -53,12 +150,12 @@ app.get('/bulkload_v2', (req, res) => {
               // ...
             });
 
-            res.send("done");
+            res.send("bulkload_v2");
 
     });
 });
 
-//Solution 1
+//Solution 1 - use metho9d batch.on
 app.get('/bulkload_v1', (req, res) => {
     const str="Query More - ";
 
@@ -103,7 +200,7 @@ app.get('/bulkload_v1', (req, res) => {
             });
             batch.on("queue", function(batchInfo) { // fired when batch request is queued in server.
                 console.log('queue, batchInfo:', batchInfo);
-                batch.poll(1000 /* interval(ms) */, 20000 /* timeout(ms) */); // start polling - Do not poll until the batch has started
+                batch.poll(1000 /* interval(ms) */, 25000 /* timeout(ms) */); // start polling - Do not poll until the batch has started
             });
             batch.on("response", function(rets) { // fired when batch finished and result retrieved
                 for (var i=0; i < rets.length; i++) {
@@ -117,7 +214,7 @@ app.get('/bulkload_v1', (req, res) => {
                 }
                 // ...
             });
-            res.send("done");
+            res.send("bulkload_v1");
 
     });
 });
